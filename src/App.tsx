3bfc13db9638a7834adfcbba501e6bdc32b8cf5b
sou-xettri <rakeshchhetri420@gmail.com>
2024-01-27 01:10:24 +0545

    Add: new data binding scheduler with simple array

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import 'devextreme/dist/css/dx.light.css';
-import Overview from './components/schedule/overview/Overview';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "devextreme/dist/css/dx.light.css";
+import Overview from "./components/schedule/overview/Overview";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SimpleArray from "./components/schedule/dataBinding/simpleArray/SimpleArray";
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/">
-      <Route index element={<Overview />} />
+        <Route index element={<Overview />} />
+        <Route path="simple-array" element={<SimpleArray />} />
       </Route>
     </Routes>
   </Router>
