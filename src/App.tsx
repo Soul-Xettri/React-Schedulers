f98e437954e384767b0f0dfa823cd542f07e8932
sou-xettri <rakeshchhetri420@gmail.com>
2024-01-28 02:42:32 +0545

    Add: 404 not found page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SimpleArray from "./components/schedule/dataBinding/simpleArray/SimpleArray";
 import { MantineProvider } from "@mantine/core";
 import { HeaderMegaMenu } from "./components/navbar/HeaderMegaMenu";
+import { NotFoundTitle } from "./pages/errorPages/NotFoundTitle";
 
 const App = () => (
   <MantineProvider>
@@ -14,6 +15,7 @@ const App = () => (
         <Route path="/">
           <Route index element={<Overview />} />
           <Route path="simple-array" element={<SimpleArray />} />
+          <Route path="*" element={<NotFoundTitle/>}/>
         </Route>
       </Routes>
     </Router>
