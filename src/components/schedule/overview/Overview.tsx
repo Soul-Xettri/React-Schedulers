98dd91023cc61e306ecf79ab4376caf7a90b345f
sou-xettri <rakeshchhetri420@gmail.com>
2024-01-29 01:20:32 +0545

    Update: overview section

--- src/components/schedule/overview/Overview.tsx
+++ src/components/schedule/overview/Overview.tsx
@@ -1,35 +1,81 @@
-import Scheduler, { Resource, SchedulerTypes } from "devextreme-react/cjs/scheduler";
-import { data, employees } from "./data";
-import ResourceCell from "./ResourceCell";
-import DataCell from "./DataCell";
+import Scheduler, {
+  Resource,
+  SchedulerTypes,
+} from "devextreme-react/cjs/scheduler";
+import { data, employees } from "./data.ts";
+import ResourceCell from "./ResourceCell.tsx";
+import DataCell from "./DataCell.tsx";
+import { Text, Title } from "@mantine/core";
 
-const currentDate = new Date(2024,5,2,11,30);
-const groups = ['employeeID'];
-const views: SchedulerTypes.ViewType[]=['month']
+const currentDate = new Date();
+const groups = ["employeeID"];
+const views: SchedulerTypes.ViewType[] = [
+  "agenda",
+  "day",
+  "month",
+  "timelineDay",
+  "timelineMonth",
+  "timelineWeek",
+  "timelineWorkWeek",
+  "week",
+  "workWeek",
+];
 
 export default function Overview() {
   return (
-    <Scheduler
-    timeZone="America/Los_Angeles"
-    dataSource={data}
-    dataCellComponent={DataCell}
-    resourceCellComponent={ResourceCell}
-    groups={groups}
-    views={views}
-    defaultCurrentDate={currentDate}
-    defaultCurrentView="month"
-    height={600}
-    showAllDayPanel={true}
-    firstDayOfWeek={1}
-    startDayHour={8}
-    endDayHour={18}
-    >
+    <div className="pr-10 pl-10">
+      <Title size={"40px"} pb={"10px"}>
+        Overview
+      </Title>
+      <Text pb={"xs"}>
+          <p>
+            DevExtreme React Scheduler is a UI component for scheduling. It
+            implements all the features necessary for its purpose: flexible data
+            binding, easy appointment editing, multiple calendar views, time
+            zones support, and more.{" "}
+            <a style={{ color: "#09b5e1", textDecoration: "underline"}} 
+              href="https://js.devexpress.com/React/Documentation/Guide/React_Components/DevExtreme_React_Components/"
+              target="_blank"
+            >
+              Learn more about DevExtreme React components
+            </a>
+            .
+          </p>
+          <p>
+            To get started with the DevExtreme Scheduler component, refer to the
+            following tutorial for step-by-step instructions:{" "}
+            <a style={{ color: "#09b5e1", textDecoration: "underline"}} 
+              href="https://js.devexpress.com/React/Documentation/Guide/UI_Components/Scheduler/Getting_Started_with_Scheduler/"
+              target="_blank"
+            >
+              Getting Started with Scheduler
+            </a>
+            .
+          </p>
+      </Text>
+      <Scheduler
+        timeZone="America/Los_Angeles"
+        dataSource={data}
+        dataCellComponent={DataCell}
+        resourceCellComponent={ResourceCell}
+        groups={groups}
+        views={views}
+        defaultCurrentView="month"
+        defaultCurrentDate={currentDate}
+        height={600}
+        showAllDayPanel={true}
+        firstDayOfWeek={0}
+        startDayHour={0}
+        endDayHour={24}
+        editing={false}
+      >
         <Resource
-            label="Employee"
-            fieldExpr="employeeID"
-            dataSource={employees}
-            allowMultiple={false}
+          label="Employee"
+          fieldExpr="employeeID"
+          dataSource={employees}
+          allowMultiple={false}
         />
-    </Scheduler>
-  )
+      </Scheduler>
+    </div>
+  );
 }
